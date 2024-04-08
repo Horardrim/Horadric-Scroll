aa7bbd92efc60e062e08e498d595c9ea09eae20e
Tal Rasha <524948250@qq.com>
2024-04-08 21:17:48 +0800

    use decorator to log when test start (#1)

--- tests/test_Demo.py
+++ tests/test_Demo.py
@@ -1,7 +1,23 @@
 import unittest
+import logging
+from functools import wraps
 
 from algo import demo
 
+logging.basicConfig(level=logging.DEBUG,
+    format="%(asctime)s %(levelname)s %(message)s",
+    datefmt = '%Y-%m-%d-%H:%M:%S')
+
 class TestDemo(unittest.TestCase):
+
+    def _test_decorator(test_func):
+        @wraps(test_func)
+        def decorator(args):
+            logging.info(test_func.__name__ + " started...")
+            return test_func(args)
+
+        return decorator
+
+    @_test_decorator
     def test_normal_case(self):
         self.assertEqual(demo.add(2, 2), 4)
